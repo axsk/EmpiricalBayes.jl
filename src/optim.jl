678537ab6868f4ddd69978ab667b0623f06b6e5d
axsk <smartmind23@googlemail.com>
2017-06-07 08:51:49 +0200

    refactor optimization routine

--- src/optim.jl
+++ src/optim.jl
@@ -1,142 +1,86 @@
 using NLopt
+using Parameters
 
-
-OPT_LOWERXB = 1e-12
-OPT_XTOLREL = 1e-5
-OPT_XTOLABS = 0
-OPT_FTOLREL = 1e-5
-OPT_CTOLABS = 1e-5
-OPT_DEBUG   = false
-OPT_MAXEVAL = 0
-
-mple(m, reg) = optimineq(m, reg)
-
-uniformweights(n) = fill(1/n, n)
-
-" optimize the models objective using dmple_obj for derivatives, with simplex inequality constraints via augmented lagrangian"
-function optimauglag(m, reg, w0 = uniformweights(length(m.xs)); optimizer=:LD_MMA, maxeval=20)
-
-    n = length(w0)
-    
-
-    ###  functions 
-    f  = mple_obj(m,reg)
-    df = dmple_obj(m,reg)
-
-
-    function myf(x,g)
-        OPT_DEBUG && @printf("f: sum(x)=%f f(x)=%f outliers=%d \n", sum(x), -f(x), sum(x.<0) + sum(x.>1))
-        if length(g) == n
-            g[:] = -df(x) # - because of minimization
-        else
-            OPT_DEBUG && print("grad length $(length(g))")
-        end
-        -f(x)
-    end
-
-    # inequalities for sum(x) == 1 
-
-    function eqconst(x, grad)
-        #grad[:] = ones(grad)
-        fill!(grad, 1)
-        (sum(x) - 1)
-    end
-
-    #=function myconst2(x,grad)
-        grad[:] = -ones(grad)
-        1-sum(x)
-    end=#
-
-    ### NLopt
-
-    opt =  Opt(:LD_AUGLAG, n)
-    opt2 = Opt(optimizer,  n)
-    
-
-    lower_bounds!(opt, OPT_LOWERXB)
-    upper_bounds!(opt, 1)
-
-    equality_constraint!(opt, eqconst, OPT_CTOLABS)
-
-
-    min_objective!(opt, myf)
-    #min_objective!(opt2, myf)
-
-    #maxeval!(opt, 100)
-    #maxeval!(opt2, 10)
-
-    #ftol_abs!(opt2, 1)
-    #ftol_abs!(opt, 1)
-    ftol_rel!(opt, OPT_FTOLREL)
-    ftol_rel!(opt2, OPT_FTOLREL)
-
-    maxeval!(opt2, maxeval)
-    xtol_rel!(opt, OPT_XTOLREL)
-    xtol_rel!(opt2, OPT_XTOLREL)
-    xtol_abs!(opt, OPT_XTOLABS)
-    xtol_abs!(opt2, OPT_XTOLABS)
-
-    local_optimizer!(opt, opt2)
-
-    minf, minx, ret = optimize(opt, w0)
-
-    if minx == w0
-        warn("Optimizer returned initial proposal")
-    end
-
-    minx
+@with_kw type OptConfig
+    LOWERXB = 1e-12
+    XTOLREL = 1e-5
+    XTOLABS = 0
+    FTOLREL = 1e-5
+    CTOLABS = 1e-5
+    DEBUG   = false
+    MAXEVAL = 0
+    METHOD = :ineq
+    OPTIMIZER = :LD_MMA
 end
 
+uniformweights(m::LikelihoodModel) = uniformweights(length(m.xs))
+uniformweights(n) = fill(1/n, n)
 
-" optimize the models objective using dmple_obj for derivatives, with simplex inequality constraints as solver constraints"
-function optimineq(m, reg, w0=uniformweights(length(m.xs)); optimizer=:LD_MMA)
+function mple(m, reg, w0=uniformweights(m); config=OptConfig())
+    @unpack_OptConfig config
 
-    n = length(w0)
-    
-
-    ###  functions 
+    # generate closures
     f  = mple_obj(m,reg)
     df = dmple_obj(m,reg)
-
-
-    function myf(x,g)
-        OPT_DEBUG && @printf("f: sum(x)=%f f(x)=%f outliers=%d \n", sum(x), -f(x), sum(x.<0) + sum(x.>1))
+    
+    function objective(x,g)
+        # note the sign due to minimizing solver
         if length(g) == n
-            g[:] = -df(x) 
+            g[:] = -df(x)
         end
+        fx = f(x)
+        DEBUG && @printf("f: f(x)=%f sum(x)=%f \n", fx, sum(x))
         -f(x)
     end
 
-    function myineq(x,g)
-        if length(g) == n
-            g[:] = 1
-        end
-        sum(x) - 1. - OPT_CTOLABS/2
-    end
+    local opt
 
-    function myineq2(x,g)
-        if length(g) == n
-            g[:] = -1
-        end
-        1. - sum(x) - OPT_CTOLABS/2 
-    end
+    n = length(w0)
+    opt = Opt(OPTIMIZER, n)
 
-    opt = Opt(optimizer,  n)
-
-    lower_bounds!(opt, OPT_LOWERXB)
+    min_objective!(opt, objective)
+    lower_bounds!(opt, LOWERXB)
     upper_bounds!(opt, 1)
 
-    inequality_constraint!(opt, myineq, OPT_CTOLABS/2)
-    inequality_constraint!(opt, myineq2, OPT_CTOLABS/2)
+    xtol_rel!(opt, XTOLREL)
+    xtol_abs!(opt, XTOLABS)
+    ftol_rel!(opt, FTOLREL)
 
-    min_objective!(opt, myf)
+    maxeval!(opt, MAXEVAL)
 
-    xtol_rel!(opt, OPT_XTOLREL)
-    xtol_abs!(opt, OPT_XTOLABS)
+    if METHOD == :ineq
+        myineq(x, g)  = ((length(g) == n) && (g[:] = -1); 
+                         sum(x) - 1. - CTOLABS/2)
+        myineq2(x, g) = ((length(g) == n) && (g[:] = -1); 
+                         1. - sum(x) - CTOLABS/2)
 
-    ftol_rel!(opt, OPT_FTOLREL)
-    maxeval!(opt, OPT_MAXEVAL)
+        inequality_constraint!(opt, myineq, CTOLABS/2)
+        inequality_constraint!(opt, myineq2, CTOLABS/2)
 
+    elseif METHOD == :auglag
+
+        function eqconst(x, grad)
+            fill!(grad, 1)
+            sum(x) - 1
+        end
+
+        # store inner optimizer and overwrite with outer
+        inneropt = opt
+        opt  = Opt(:LD_AUGLAG, n)
+
+        min_objective!(opt, objective)
+        lower_bounds!(opt, LOWERXB)
+        upper_bounds!(opt, 1)
+
+        equality_constraint!(opt, eqconst, CTOLABS)
+
+
+        xtol_rel!(opt, XTOLREL)
+        xtol_abs!(opt, XTOLABS)
+        ftol_rel!(opt, FTOLREL)
+
+        local_optimizer!(opt, inneropt)
+    end
 
     minf, minx, ret = optimize(opt, w0)
 
